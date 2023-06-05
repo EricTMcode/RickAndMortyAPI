61a4e23908cba228ac7dcb0e7ce53cfca55bdf26
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-06-05 13:50:59 +0200

    - Create CharacterViewModel

---
+++ RickAndMortyAPI/ViewModel/CharacterViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  CharacterViewModel.swift
+//  RickAndMortyAPI
+//
+//  Created by Eric on 05/06/2023.
+//
+
+import Foundation
+
+@MainActor
+class CharacterViewModel: ObservableObject {
+    
+    enum State {
+        case na
+        case loading
+        case success(data: [Character])
+        case failed(error: Error)
+    }
+    
+    @Published private(set) var state: State = .na
+    
+    private let service: CharacterService
+    
+    init(service: CharacterService) {
+        self.service = service
+    }
+    
+    func getCharacters() async {
+        
+        self.state = .loading
+        
+        do {
+            let characters = try await service.fetchCharacters()
+            self.state = .success(data: characters)
+        } catch {
+            self.state = .failed(error: error)
+        }
+    }
+}
