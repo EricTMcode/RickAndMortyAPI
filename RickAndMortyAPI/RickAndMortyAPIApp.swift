84815e14e26bc5c10230cc448e4d5fe8a02aa802
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-06-05 13:24:16 +0200

    Initial Commit

---
+++ RickAndMortyAPI/RickAndMortyAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAPIApp.swift
+//  RickAndMortyAPI
+//
+//  Created by Eric on 05/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
